48e189d0436b0754ccde013f2f878f6fe494f9f3
yandrakarine <yandrakarine.dev@outlook.com>
2024-03-27 21:44:34 -0300

    feat(add-repository): add constants for repository

---
+++ src/repository/constants.ts
@@ -0,0 +1,8 @@
+enum OpenWeatherEndPoints {
+  GeoCoding = '/geo/1.0/direct?',
+  DailyForecast = '/data/2.5/forecast?',
+}
+
+const RESULTS_LIMIT = 5;
+
+export { OpenWeatherEndPoints, RESULTS_LIMIT };
