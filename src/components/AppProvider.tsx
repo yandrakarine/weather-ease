fc76c0c909010fd8c1d96141814d8cb01fa23cb4
Yandra Karine <yandrakarine.dev@hotmail.com>
2024-03-27 21:31:15 -0300

    chore(project-setup): add react-query
    add react query to project (https://tanstack.com/query/latest)

--- src/components/AppProvider.tsx
+++ src/components/AppProvider.tsx
@@ -1,5 +1,6 @@
 import { ThemeProvider } from '@shopify/restyle';
 import theme from '@styles/theme';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import React from 'react';
 import { ReactNode } from 'react';
 
@@ -7,8 +8,14 @@ type AppProviderProps = {
   children: ReactNode;
 };
 
+const queryClient = new QueryClient();
+
 const AppProvider = ({ children }: AppProviderProps) => {
-  return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>{children}</ThemeProvider>
+    </QueryClientProvider>
+  );
 };
 
 export { AppProvider };
