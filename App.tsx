3b30b2c098e667b0545760a1918e35ae37af7142
yandrakarine <yandrakarine.dev@outlook.com>
2024-03-29 17:57:27 -0300

    feat(city-suggestions): add app statusbar

--- App.tsx
+++ App.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import { Navigation } from '@navigation/Navigation';
 import { AppProvider } from '@components/AppProvider';
+import { StatusBar } from 'react-native';
+import theme from '@styles/theme';
 
 const App = () => (
   <AppProvider>
+    <StatusBar
+      barStyle={'light-content'}
+      backgroundColor={theme.colors.mainBackground}
+    />
     <Navigation />
   </AppProvider>
 );
