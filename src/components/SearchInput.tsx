874fcfdde66530878cb711ab54b3dd881c822d35
yandrakarine <yandrakarine.dev@outlook.com>
2024-03-29 18:03:28 -0300

    feat(city-suggestions): add styles to search input component

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,7 +1,7 @@
 import { TextInputProps } from 'react-native/types';
-import { TextInput, StyleSheet } from 'react-native';
+import { TextInput } from 'react-native';
 import { Container } from './Container';
-import React from 'react';
+import React, { useMemo } from 'react';
 import {
   BackgroundColorProps,
   SpacingProps,
@@ -13,9 +13,11 @@ import {
   LayoutProps,
   border,
   BorderProps,
+  useTheme,
 } from '@shopify/restyle';
 import { Theme } from '@styles/theme';
 import { Icon } from './Icon';
+import { FontName } from '@assets/fonts/fontName';
 
 type RestyleProps = SpacingProps<Theme> &
   BackgroundColorProps<Theme> &
@@ -29,21 +31,34 @@ const restyleFunctions = composeRestyleFunctions<Theme, RestyleProps>([
   border,
 ]);
 
-const styles = StyleSheet.create({
-  input: {
-    flexGrow: 1,
-  },
-});
-
 type SearchInputProps = RestyleProps & TextInputProps;
 
 const SearchInput = (searchInputProps: SearchInputProps) => {
   const props = useRestyle(restyleFunctions, searchInputProps);
 
+  const theme = useTheme<Theme>();
+
+  const styles = useMemo(
+    () => ({
+      input: {
+        flexGrow: 1,
+        fontFamily: FontName['Poppins-Regular'],
+        color: theme.colors.iconGrey,
+      },
+    }),
+    [theme.colors.iconGrey],
+  );
+
   return (
     <Container height={50} flexDirection="row" alignItems="center" {...props}>
       <Icon name="search" width={20} height={20} color="iconGrey" />
-      <TextInput placeholder={props.placeholder} style={styles.input} />
+      <TextInput
+        placeholderTextColor={theme.colors.iconGrey}
+        placeholder={props.placeholder}
+        onChangeText={props.onChangeText}
+        style={styles.input}
+        value={props.value}
+      />
     </Container>
   );
 };
