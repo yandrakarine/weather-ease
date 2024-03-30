06d76d10714ee4ed459fe76ee3c7e129519bf594
yandrakarine <yandrakarine.dev@outlook.com>
2024-03-29 23:08:24 -0300

    feat(weather-forecast): add util absolute path

--- babel.config.js
+++ babel.config.js
@@ -12,6 +12,7 @@ module.exports = {
           '@screens': './src/screens',
           '@repository': './src/repository',
           '@assets': './src/assets',
+          '@utils': './src/utils',
         },
       },
     ],
