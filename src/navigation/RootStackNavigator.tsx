5421ef68ec453e1d93d9435bf46628c4594fe424
yandrakarine <yandrakarine.dev@outlook.com>
2024-03-29 23:11:00 -0300

    feat(weather-forecast): add root stack header component

--- src/navigation/RootStackNavigator.tsx
+++ src/navigation/RootStackNavigator.tsx
@@ -1,30 +1,56 @@
-import React from 'react';
+import { RootStackHeader } from '@components/RootStackHeader';
+import { Visibility } from '@components/Visibility';
 import {
+  NativeStackHeaderProps,
   NativeStackNavigationOptions,
   createNativeStackNavigator,
 } from '@react-navigation/native-stack';
-import { HomeScreen } from '@screens/HomeScreen';
 import { DetailsScreen } from '@screens/DetailsScreen';
+import { HomeScreen } from '@screens/HomeScreen';
+import React from 'react';
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
-const getScreenOptions = (
-  withoutHeader?: boolean,
-): NativeStackNavigationOptions => ({
+const getScreenOptions = ({
+  withoutHeader,
+  navigation,
+  title,
+}: {
+  title?: string;
+  withoutHeader?: boolean;
+  navigation: NativeStackHeaderProps['navigation'];
+}): NativeStackNavigationOptions => ({
   presentation: 'card',
   headerTitleAlign: 'center',
   headerBackVisible: false,
   title: '',
-  ...(withoutHeader && { header: () => null }),
+  header: () => (
+    <Visibility isVisible={!withoutHeader}>
+      <RootStackHeader navigation={navigation} title={title} />
+    </Visibility>
+  ),
 });
 
 const RootStackNavigator = () => {
   return (
     <Stack.Navigator initialRouteName="HomeScreen">
-      <Stack.Group screenOptions={() => getScreenOptions(true)}>
+      <Stack.Group
+        screenOptions={({ navigation }) =>
+          getScreenOptions({
+            withoutHeader: true,
+            navigation,
+          })
+        }>
         <Stack.Screen name="HomeScreen" component={HomeScreen} />
       </Stack.Group>
-      <Stack.Group screenOptions={() => getScreenOptions(false)}>
+      <Stack.Group
+        screenOptions={({ navigation }) =>
+          getScreenOptions({
+            navigation,
+            withoutHeader: false,
+            title: 'Details',
+          })
+        }>
         <Stack.Screen name="DetailsScreen" component={DetailsScreen} />
       </Stack.Group>
     </Stack.Navigator>
